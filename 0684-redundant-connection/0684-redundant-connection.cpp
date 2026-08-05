48a9774db50acc633b828ebb6d56d350f545f38f
Dingle Varshney <121789385+DingleVarshney12@users.noreply.github.com>
2026-08-05 21:10:52 +0530

    Time: 0 ms (100%), Space: 13.2 MB (72%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,31 +1,42 @@
 class Solution {
-    bool DFS(int node ,int target,vector<vector<int>>& adj,vector<int>& visited){
-        if(node == target) return true;
-        visited[node] = 1;
-        for(auto neighbour : adj[node]){
-            if(!visited[neighbour]){
-                if(DFS(neighbour,target,adj,visited)){
-                    return true;
-                }
-            }
-        }
-        return false;
-        
+    vector<int> parent,size;
+     int findParent(int node){
+        if(parent[node] == node) return node;
+        return parent[node]= findParent(parent[node]);
     }
+    void unionBySize(int u,int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] > size[ulp_v]){
+            parent[ulp_v] = ulp_u;
+            size[ulp_u]+= size[ulp_v];
+        }else{
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+    }
+
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size() +1;
-        vector<vector<int>> adj(n+1);
-        for(auto edge:edges){
+        int n = edges.size();
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for(int i = 0;i <= n;i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+        for(auto &edge: edges){
             int u = edge[0];
             int v = edge[1];
-            vector<int> visited(n+1,0);
-            if(DFS(u,v,adj,visited)){
-                return edge;
+            int ulp_u = findParent(u);
+            int ulp_v = findParent(v);
+
+            if(ulp_u == ulp_v){
+                return {u,v};
             }
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            unionBySize(u,v);
         }
-        return {};
+            return {};
     }
 };
