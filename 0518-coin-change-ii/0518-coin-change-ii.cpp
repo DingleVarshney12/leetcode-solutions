0be033004390f4eb7b69de527333139a085b9784
Dingle Varshney <121789385+DingleVarshney12@users.noreply.github.com>
2026-08-13 20:33:38 +0530

    Time: 7 ms (87.37%), Space: 10.3 MB (91.23%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<double> dp(amount+1,0);
-        dp[0] =1;
-        for(int i = n-1;i>=0;i--){
-            for(int j =1;j<=amount;j++){
-                if(coins[i] <= j) {
-                    dp[j] = dp[j-coins[i]] + dp[j];
-                }
+
+        vector<long long> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for(int coin : coins) {
+
+            for(int j = coin; j <= amount; j++) {
+
+                dp[j] = min(
+                    (long long)INT_MAX,
+                    dp[j] + dp[j - coin]
+                );
             }
         }
+
         return dp[amount];
     }
 };
