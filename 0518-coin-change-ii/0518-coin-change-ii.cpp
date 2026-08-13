8439c3322de3ec073ea5fa50976407c3154ff7e3
Dingle Varshney <121789385+DingleVarshney12@users.noreply.github.com>
2026-08-13 20:24:19 +0530

    Time: 8 ms (81.9%), Space: 10.3 MB (91.23%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<double> dp(amount+1,0);
+        dp[0] =1;
+        for(int i = n-1;i>=0;i--){
+            for(int j =1;j<=amount;j++){
+                if(coins[i] <= j) {
+                    dp[j] = dp[j-coins[i]] + dp[j];
+                }
+            }
+        }
+        return dp[amount];
+    }
+};
