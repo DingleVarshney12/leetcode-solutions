6ebe20e5a04192f65924add841bf9997e4bb1a91
Dingle Varshney <121789385+DingleVarshney12@users.noreply.github.com>
2026-09-02 21:22:47 +0530

    Time: 0 ms (100%), Space: 11.6 MB (60.88%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,17 +2,27 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        for(int i = n-1 ; i >=0 ;i--){
+
+        // hum traversal right se start karenge kyuki addition last digit pe hoga
+        for(int i = n - 1; i >= 0; i--) {
+
+            // agr current digit 9 nahi hai toh increment kardo 
+            // aagr carry nhi aayi toh return kardo array
             if(digits[i] != 9){
                 digits[i] += 1;
                 return digits;
             }
+
+            // aagr digit 9 aayi toh digit ko 0 karke carry ko left mein move kardo 
             digits[i] = 0;
+
+            // aagr hum first digit pe aa gye toh mtlb sari digits 9 thi isliye hum aage 1 insert kar denge
             if(i == 0){
-                digits.insert(digits.begin(),1);
+                digits.insert(digits.begin(), 1);
                 return digits;
             }
         }
+
         return digits;
     }
 };
