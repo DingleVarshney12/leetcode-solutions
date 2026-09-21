90019b9a60d02c7885263948357310116ec6c95a
Dingle Varshney <121789385+DingleVarshney12@users.noreply.github.com>
2026-09-21 21:54:43 +0530

    Time: 111 ms (46.27%), Space: 60.9 MB (69.4%) - LeetHub

--- 2512-reward-top-k-students/2512-reward-top-k-students.cpp
+++ 2512-reward-top-k-students/2512-reward-top-k-students.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
+    /**
+    * Compare score, and if same then compare student_id.
+    * Worst student stays at the top.
+    */
     struct Compare {
         bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
 
-            if (a.first == b.first)
-                return a.second > b.second;
-
-            return a.first < b.first;
+            if (a.first != b.first)
+                return a.first > b.first; 
+ 
+            return a.second < b.second;
         }
     };
 
@@ -15,17 +19,22 @@ public:
                             vector<string>& report, vector<int>& student_id,
                             int k) {
 
+        /**
+        * Hash Set for fast Looks Up 
+        */
         unordered_set<string> pos(positive_feedback.begin(),
                                   positive_feedback.end());
 
         unordered_set<string> neg(negative_feedback.begin(),
                                   negative_feedback.end());
 
+
         priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
 
         for (int i = 0; i < student_id.size(); i++) {
 
             int score = 0;
+            // split string by space
             istringstream iss(report[i]);
             string word;
 
@@ -38,15 +47,21 @@ public:
             }
 
             pq.push({score, student_id[i]});
+
+            // fixed k size priority queue 
+            if (pq.size() > k)
+                pq.pop();
         }
 
         vector<int> ans;
 
-        while (k--) {
+        while (!pq.empty()) {
             ans.push_back(pq.top().second);
             pq.pop();
         }
 
+        reverse(ans.begin(), ans.end());
+        
         return ans;
     }
 };
